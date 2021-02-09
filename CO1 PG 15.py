2a348b97d868445a29bc6469f917db1327f83eaf
bhavya365 <76951782+bhavya365@users.noreply.github.com>
2021-02-09 06:07:45 -0800

    Add files via upload

---
+++ CO1 PG 15.py
@@ -0,0 +1,15 @@
+n=int(input("Enter a limit:"))
+a=[]
+b=[]
+c[]
+print("Enter 1st and 2nd list:")
+for i in range(0,n):
+    print("1st:",end="")
+    a.append(input())
+    print("2nd:",end="")
+    b.append(input())
+for i in range(0,n):
+    if a[i]==b[i]:
+        continue
+    else:
+        print(a[i])

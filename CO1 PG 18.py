fb5366f4cf74ce6260f59253bc49f214cc5d2a3f
bhavya365 <76951782+bhavya365@users.noreply.github.com>
2021-02-09 06:10:52 -0800

    Add files via upload

---
+++ CO1 PG 18.py
@@ -0,0 +1,18 @@
+from collections import OrderedDict
+d={}
+di={}
+a=int(input("Enter a limit:"))
+for i in range(0,a):
+    k=int(input("Enter key:"))
+    v=int(input("Enter value:"))
+    d.update({k:v})
+b=int(input("\nEnter a limit:"))
+for i in range(0,b):
+    ke=int(input("Enter key:"))
+    vv=int(input("Enter value:"))
+    di.update({ke:vv})
+print("Before merging:",d,di,end=" ")
+s={}
+s.update(d)
+s.update(di)
+print("\nAfter merging:",s)

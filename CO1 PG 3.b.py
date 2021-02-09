9e7df4e2bb2c35568af21afd4299110863a54fbf
bhavya365 <76951782+bhavya365@users.noreply.github.com>
2021-02-09 05:42:22 -0800

    Add files via upload

---
+++ CO1 PG 3.b.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a limit:"))
+lst=[]
+print(f"Enter {n} numbers")
+for i in range(1,n+1):
+    a=int(input())
+    lst.append(a)
+for i in range(0,n):
+     print(lst[i]*lst[i])
+
+
+
+

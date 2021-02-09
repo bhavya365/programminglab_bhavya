fa288834e61ac6b54453edb13890baa7c89a6208
bhavya365 <76951782+bhavya365@users.noreply.github.com>
2021-02-09 05:56:33 -0800

    Add files via upload

---
+++ CO1 PG 11.py
@@ -0,0 +1,15 @@
+a=int(input("a:"))
+b=int(input("b:"))
+c=int(input("c:"))
+if a>b:
+    if a>c:
+        print(a ," a is greater")
+    else:
+        print(c ," c is greater")
+elif b>a:
+    if b>c:
+        print(b ," b Is greater")
+    else:
+        print(c ," c is greater")
+else:
+    print(c ," c is greater")
